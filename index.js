b90f36bae71fd47f0cb3c63c21f47e697831b948
Sean Kipina <paintballsean@gmail.com>
2022-09-09 01:13:06 +0300

    finished all the main tests

--- index.js
+++ index.js
@@ -275,8 +275,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if(score >= 90){
+    return 'you got an A';
+  }else if(score >= 80){
+    return 'you got a B';
+  }else if(score >= 70){
+    return 'you got a C';
+  }else if(score >= 60){
+    return 'you got a D';
+  }else{
+    return 'you got an F';
+  }
 }
 
 
